fc3f7dc4ff7a3e4b579452e2759d750b1eec2f29
kaechle <timothy.kaechle@me.com>
2022-05-07 18:38:38 -0300

    markdown conversion using marked

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
+  mode: "jit",
+  purge: ["./dist/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   content: ["./src/*.{html,js}"],
   theme: {
     extend: {}
